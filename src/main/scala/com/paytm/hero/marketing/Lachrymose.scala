e06539fd6e2db83bde7806b7a3a3a63d7b054a98
Adam Muise <adammuise@mentat.home>
2017-01-27 23:04:20 -0500

    Switched to java's Instant class for timestamp

--- src/main/scala/com/paytm/hero/marketing/Lachrymose.scala
+++ src/main/scala/com/paytm/hero/marketing/Lachrymose.scala
@@ -4,7 +4,7 @@ package com.paytm.hero.marketing
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.{SparkConf, SparkContext}
-import java.util.Calendar
+import java.time.Instant
 
 object Lachrymose {
   def main(args: Array[String]) {
@@ -12,7 +12,7 @@ object Lachrymose {
     val ga_path = s"/workspace/midgar/prod/base/ga_sanitized/"
     val oauth_snap = "/apps/hive/warehouse/oauth.db/customer_registration_snapshot_v2"
     val output_path = s"/tmp/adam"
-    val txt_output = "/tmp/adam-count" + Calendar.getInstance().get(Calendar.SECOND) + ".txt"
+    val txt_output = "/tmp/adam-count" + Instant.now.getEpochSecond.toString + ".txt"
     val hdfs_user = "adam"
     val hdfs_connect_string = "hdfs://labshdpds2"
 
